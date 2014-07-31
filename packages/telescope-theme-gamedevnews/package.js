cbc4e96fc70a44ecb34391586746a0ab9c893049
Devon Campbell <raddevon@gmail.com>
2014-07-31 13:14:11 -0400

    Adds files necessary for logos

--- packages/telescope-theme-gamedevnews/package.js
+++ packages/telescope-theme-gamedevnews/package.js
@@ -2,7 +2,7 @@ Package.describe({summary: "Telescope GameDevNews theme"});
 
 Package.on_use(function (api) {
 
-  api.use(['telescope-theme-hubble'], ['client']);
+  api.use(['templating', 'telescope-base','telescope-theme-hubble'], ['client']);
 
   api.add_files([
     'lib/client/stylesheets/screen.css',
@@ -10,6 +10,12 @@ Package.on_use(function (api) {
     'lib/client/stylesheets/fonts/icomoon.svg',
     'lib/client/stylesheets/fonts/icomoon.ttf',
     'lib/client/stylesheets/fonts/icomoon.woff',
+    'lib/client/img/gamedevnews.png',
+    'lib/client/img/gamedevnews.svg',
+    'lib/client/img/gdn.svg',
+    'lib/client/img/gdn.png',
+    'lib/client/templates/new_nav.html',
+    'lib/client/gamedevnews.js'
     ], ['client']);
 
 });
