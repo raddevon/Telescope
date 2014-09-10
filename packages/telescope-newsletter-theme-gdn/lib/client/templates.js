3814469b451b04dd54d2b859440e89e95632747c
Devon Campbell <raddevon@gmail.com>
2014-09-10 08:52:26 -0400

    Completes styling for subscribe form

---
+++ packages/telescope-newsletter-theme-gdn/lib/client/templates.js
@@ -0,0 +1,1 @@
+templates['newsletterBanner'] = newNewsletterBanner;
--- packages/telescope-newsletter-theme-gdn/package.js
+++ packages/telescope-newsletter-theme-gdn/package.js
@@ -2,11 +2,12 @@ Package.describe({summary: "Telescope Newsletter GDN theme"});
 
 Package.on_use(function (api) {
 
-  api.use(['telescope-newsletter'], ['client']);
+  api.use(['telescope-newsletter', 'telescope-base', 'telescope-lib'], ['client']);
 
   api.add_files([
+    'lib/client/newsletter_banner.html',
     'lib/client/newsletter_banner.css',
-    'lib/client/bannerTemplate.html'
+    'lib/client/templates.js'
     ], ['client']);
 
 });
