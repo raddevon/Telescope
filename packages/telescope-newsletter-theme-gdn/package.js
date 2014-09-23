8c33392d511917d3e8b87fef5707783896289159
Devon Campbell <raddevon@gmail.com>
2014-09-23 18:13:32 -0400

    Installs sitemaps package

--- packages/telescope-newsletter-theme-gdn/package.js
+++ packages/telescope-newsletter-theme-gdn/package.js
@@ -2,7 +2,7 @@ Package.describe({summary: "Telescope Newsletter GDN theme"});
 
 Package.on_use(function (api) {
 
-  api.use(['telescope-newsletter', 'telescope-base', 'telescope-lib'], ['client']);
+  api.use(['templating', 'spacebars', 'ui', 'telescope-lib', 'telescope-base', 'telescope-newsletter'], ['client']);
 
   api.add_files([
     'lib/client/newsletter_banner.html',
